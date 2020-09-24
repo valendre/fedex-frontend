64f9734c127ba7f9e9504940ac8f69671f6a3d4a
Keszler Barnabás <keszler.barnabas@gmail.com>
2020-09-24 18:43:36 +0200

    feat(gomb) added annoying af button

--- src/components/Cohorts.js
+++ src/components/Cohorts.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+import {useSelector} from 'react-redux';
+
 import useRequest from "../hooks/useRequest";
 import CohortSingleton from './CohortSingleton';
 
@@ -6,6 +8,8 @@ export default function (props) {
   const [requestWithToken] = useRequest();
   const [cohortsList, setCohortsList] = useState([]);
   const [selectedValue, setSelectedValue] = useState("");
+  const authFromRedux = useSelector(state => state.auth);
+
   
   useEffect(() => {
     async function fetchCohorts() {
@@ -30,6 +34,20 @@ export default function (props) {
         ))}
       </select>
       {selectedValue ? <h2>{cohortsList[selectedValue-1].cohort_name}</h2> : null}
+      {selectedValue ? <button onClick={async() => {
+         let req = await fetch('/cohort/download-links', {
+          method: "POST",
+          body: JSON.stringify({"cohort_id": selectedValue}),
+          headers: {
+            'Authorization': 'Bearer ' + authFromRedux.token,
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Credentials': 'true',
+            'Access-Control-Allow-Origin': 'http://localhost:8080',
+        }
+
+        })
+        console.log(req);
+      }}>!SEND EMAILS!</button> : null}
       {selectedValue ? <CohortSingleton id={selectedValue} /> : null}
     </>
   );
