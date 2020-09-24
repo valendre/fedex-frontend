0c309f9caa34d17540e5b213790aab992afd6c95
Keszler Barnabás <keszler.barnabas@gmail.com>
2020-09-24 15:28:50 +0200

    lol

--- src/components/CohortSingleton.js
+++ src/components/CohortSingleton.js
@@ -12,7 +12,7 @@ export default function (props) {
       console.log(response);
     }
     fetchCohortMembers();
-  }, []);
+  }, [props.id]);
 
   return (
     <div className="studentcontroller">
