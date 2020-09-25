8d11958cd34a9d6541ecf8dc10d72baed9cfb0ae
valendre <valendre@gmail.com>
2020-09-25 09:20:16 +0200

    student details

--- src/components/CohortSingleton.js
+++ src/components/CohortSingleton.js
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from "react";
 import useRequest from "../hooks/useRequest";
 import SockJsClient from "react-stomp";
+import Student from "./Student";
+import {logOut} from "../actions";
 
 export default function (props) {
   const [requestWithToken] = useRequest();
   const [message, setMessage] = useState({});
   const [firstLoad, setFirstLoad] = useState(true);
   const [studentList, setStudentList] = useState([]);
+  const [selectedValue, setSelectedValue] = useState(undefined);
 
   useEffect(() => {
     async function fetchCohortMembers() {
@@ -21,6 +24,7 @@ export default function (props) {
   }, [props.id, message]);
 
   return (
+      <>
     <div className="studentcontroller">
       <SockJsClient
         url="http://localhost:8080/websocket-fedex"
@@ -48,7 +52,7 @@ export default function (props) {
       <div className="notSuspicious studentColumn">
       <h3>not_suspicious</h3>
       {studentList.filter(student => student.suspiciousLevel === 0).map((student) => (
-        <div className="studentTab">
+        <div className="studentTab" onClick={() => {setSelectedValue(student.id)}}>
           <div>Id: {student.id}</div>
           <div>{student.username}</div>
         </div>
@@ -57,7 +61,7 @@ export default function (props) {
       <div className="slightlySuspicious studentColumn">
       <h3>slightly_suspicious</h3>
       {studentList.filter(student => student.suspiciousLevel === 1).map((student) => (
-        <div className="studentTab">
+          <div className="studentTab" onClick={() => {setSelectedValue(student.id)}}>
           <div>Id: {student.id}</div>
           <div>{student.username}</div>
         </div>
@@ -66,12 +70,14 @@ export default function (props) {
       <div className="suspicious studentColumn">
       <h3>very_suspicious</h3>
       {studentList.filter(student => student.suspiciousLevel === 2).map((student) => (
-        <div className="studentTab">
+          <div className="studentTab" onClick={() => {setSelectedValue(student.id)}}>
           <div>Id: {student.id}</div>
           <div>{student.username}</div>
         </div>
       ))}
       </div>
     </div>
+    {selectedValue ? <Student id={selectedValue}/> : null}
+    </>
   );
 }
--- src/components/Student.js
+++ src/components/Student.js
@@ -5,12 +5,14 @@ import SockJsClient from 'react-stomp';
 
 export default function (props) {
   const [requestWithToken] = useRequest();
-  const [studentInfo, setStudentInfo] = useState([]);
+  const [studentInfo, setStudentInfo] = useState(undefined);
+
 
   useEffect(() => {
     async function fetchStudent() {
       let response = await requestWithToken("GET", "/student/" + props.id);
-      setStudentInfo({username: "andi", processes: [{name: "shebang.exe", memoryUsage: "69 MB"}, {name: "shebang.exe", memoryUsage: "69 MB"}]});
+      //setStudentInfo({username: "andi", activities: [{name: "shebang.exe", memoryUsage: "69 MB"}, {name: "shebang.exe", memoryUsage: "69 MB"}]});
+      setStudentInfo(response);
     }
     fetchStudent();
   }, []);
@@ -18,14 +20,14 @@ export default function (props) {
   return (
     <div className="studentInfo">
       
-      <h3>{studentInfo.username}</h3>
+      <h3>{studentInfo ? studentInfo.username : null}</h3>
       <h4>Processes</h4>
       <ul>
-        {studentInfo.processes.map((process) => (
+        {(studentInfo&&studentInfo.activities) ? studentInfo.activities.map((process) => (
           <li>
             {"Name: " + process.name + "Memory usage: " + process.memoryUsage}
           </li>
-        ))}
+        )) : null}
       </ul>
     </div>
   );
